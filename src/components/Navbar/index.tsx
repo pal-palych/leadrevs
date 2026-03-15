52c4165ad2b7080f8550414e0c950d1a0fa57d4e
Pasha <pasha@Mac.lan>
2026-03-15 17:20:10 -0400

    Remove search bar from navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -155,35 +155,6 @@ export default function Navbar() {
               </div>
 
               <div className="hidden items-center justify-end gap-4 pr-16 sm:flex lg:pr-0">
-                <button
-                  onClick={() => setSearchModalOpen(true)}
-                  className="text-body-color hidden h-10 w-10 items-center justify-center rounded-full sm:flex"
-                >
-                  <svg
-                    width="24"
-                    height="24"
-                    viewBox="0 0 18 18"
-                    fill="currentColor"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <g clipPath="url(#clip0_369_1884)">
-                      <path
-                        d="M16.9347 15.3963L12.4816 11.7799C14.3168 9.26991 14.1279 5.68042 11.8338 3.41337C10.6194 2.19889 9.00003 1.52417 7.27276 1.52417C5.54549 1.52417 3.92617 2.19889 2.71168 3.41337C0.201738 5.92332 0.201738 10.0256 2.71168 12.5355C3.92617 13.75 5.54549 14.4247 7.27276 14.4247C8.91907 14.4247 10.4574 13.804 11.6719 12.6975L16.179 16.3409C16.287 16.4219 16.4219 16.4759 16.5569 16.4759C16.7458 16.4759 16.9077 16.3949 17.0157 16.26C17.2316 15.9901 17.2046 15.6122 16.9347 15.3963ZM7.27276 13.2102C5.86935 13.2102 4.5739 12.6705 3.57532 11.6719C1.52418 9.62076 1.52418 6.30116 3.57532 4.27701C4.5739 3.27843 5.86935 2.73866 7.27276 2.73866C8.67617 2.73866 9.97162 3.27843 10.9702 4.27701C13.0213 6.32815 13.0213 9.64775 10.9702 11.6719C9.99861 12.6705 8.67617 13.2102 7.27276 13.2102Z"
-                        fill="currentColor"
-                      />
-                    </g>
-                    <defs>
-                      <clipPath id="clip0_369_1884">
-                        <rect
-                          width="17.2727"
-                          height="17.2727"
-                          fill="white"
-                          transform="translate(0.363647 0.363647)"
-                        />
-                      </clipPath>
-                    </defs>
-                  </svg>
-                </button>
                 <Link
                   href="/contact"
                   className="bg-primary hover:bg-primary/90 hover:shadow-signUp rounded-full px-8 py-3 text-base font-bold whitespace-nowrap text-white transition duration-300 ease-in-out md:px-9 lg:px-8 xl:px-9"
